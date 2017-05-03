a7b5ebbf98fc4b789700759af4bca40134d88b9c
Tómas Jóhannesson <tomash14@ru.is>
2017-05-03 12:38:19 +0000

    TOMth

--- source/eleven/Controllers/HomeController.cs
+++ source/eleven/Controllers/HomeController.cs
@@ -13,13 +13,6 @@ namespace eleven.Controllers
             return View();
         }
 
-        //Virkar þetta kv. Godi
-<<<<<<< HEAD
-        //NEI kv. Tommi
-=======
-        //yass biaatchhhh 
->>>>>>> 31ed73c8ab7fe344afbc911c2b8b3814935f2922
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
