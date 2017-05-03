e0e6b02e38a1fb137c31c2934f03dcf0869dadb1
Gunnthora Mist <Gunnthora Mist@DESKTOP-O1GBUCO>
2017-05-03 13:11:43 +0000

    file and project class

---
+++ source/eleven/Models/Entities/File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eleven.Models.Entities
+{
+    public class File
+    {
+        private string name { get; set; }
+        private string type { get; set; }
+        private string content { get; set; }
+        private int fileId { get; set; }
+    }
+}
---
+++ source/eleven/Models/Entities/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eleven.Models.Entities
+{
+    public class Project
+    {
+        private string name { get; set; }
+        private int projectId { get; set; }
+
+    }
+}
