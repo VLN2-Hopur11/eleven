d5739ca4e105a088130740ef60668acd87cefd3e
Goði Daðason <Goi Daason@DESKTOP-UK050FS>
2017-05-04 15:10:08 +0000

    Highlights .cs bætt við

---
+++ source/eleven/Models/Entities/Highlights.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eleven.Models.Entities
+{
+    /// <summary>
+    /// Highlights from news.
+    /// </summary>
+    public class Highlights
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+        public string Id { get; set; }
+        public string author { get; set; }
+    }
+}
