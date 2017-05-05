39480ac7804c0e52721728ee0d1e843421ca3ccd
Gunnthora Mist <Gunnthora Mist@DESKTOP-O1GBUCO>
2017-05-05 14:20:04 +0000

    myprojectviewmodel

---
+++ source/eleven/Models/ViewModels/MyProjectViewModel.cs
@@ -0,0 +1,13 @@
+﻿using eleven.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eleven.Models.ViewModels
+{
+    public class MyProjectViewModel
+    {
+        public List<Project> projects { get; set; }
+    }
+}
