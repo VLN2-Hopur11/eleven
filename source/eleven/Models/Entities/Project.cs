f5f54528f9ec3fb5e0063801e40b5a08fb2989bd
Goði Daðason <godi15@ru.is>
2017-05-09 11:30:34 +0000

    commit - pull

--- source/eleven/Models/Entities/Project.cs
+++ source/eleven/Models/Entities/Project.cs
@@ -6,10 +6,7 @@ using System.Web;
 
 namespace eleven.Models.Entities
 {
-    /// <summary>
-    /// A project are created by user 
-    /// which can hold one or more files 
-    /// </summary>
+    /* A project are created by user which can hold one or more files */
     public class Project
     {
         public int Id { get; set; }
