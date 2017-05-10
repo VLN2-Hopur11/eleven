4728c1af7e18907659dfc2893e364df74f069aa7
rebekka123 <rebekkagudmunds@gmail.com>
2017-05-10 12:16:45 +0000

    ProjectServiceTest made

---
+++ source/eleven/eleven.Tests/Services/ProjectServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eleven.Tests.Services
+{
+    [TestClass]
+    public class ProjectServiceTest
+    {
+        [TestMethod]
+        public void TestUserExists()
+        {
+            // Arrange: const string user = "dabs";
+            // var service = new UberService();
+
+            // Act: var result = service.GetFriendsForUser(user);
+
+            // Assert:  Assert.AreEqual(2, result.Count);
+        }
+    }
+}
