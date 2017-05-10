c408dcbdac3fe460e2b1d512a9aad9a452bc9c3c
Gunnthora Mist <Gunnthora Mist@DESKTOP-O1GBUCO>
2017-05-10 10:00:54 +0000

    some craycray masterpiece

--- source/eleven/Models/ViewModels/ProjectViewModel.cs
+++ source/eleven/Models/ViewModels/ProjectViewModel.cs
@@ -9,7 +9,6 @@ namespace eleven.Models.ViewModels
     public class ProjectViewModel
     {
         public Project project { get; set; }
-        public File files { get; set; }
         public Folder folders { get; set; }
         public List<File> files { get; set; }
         public File activeFile { get; set; }
